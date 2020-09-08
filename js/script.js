4b0628cd790d1491c82129cee40af4fa4740298b
walkerashmore <ashmorsw@miamioh.edu>
2020-09-08 19:27:28 -0400

    Google Analytics Set-Up

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
 $(document).ready(function(){
     $('.casestudies').slick({
-      infinite: false,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    });
-  });
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+})
+});
